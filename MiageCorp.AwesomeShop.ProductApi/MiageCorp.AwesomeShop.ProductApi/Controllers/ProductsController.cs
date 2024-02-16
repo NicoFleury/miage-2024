cbca447d7aaf92d093660be828f138fd13bc2bd3
Nicolas FLEURY <nicolas.fleury1@soprasteria.com>
2024-02-16 21:43:09 +0100

    Solution dotnet7

---
+++ MiageCorp.AwesomeShop.ProductApi/MiageCorp.AwesomeShop.ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using MiageCorp.AwesomeShop.ProductApi.Exceptions;
+using MiageCorp.AwesomeShop.ProductApi.Models;
+using MiageCorp.AwesomeShop.ProductApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiageCorp.AwesomeShop.ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IProductService ProductService { get; set; }
+
+        public ProductsController(IProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult Get(string id)
+        {
+            var product = ProductService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            var result = ProductService.AddProduct(product);
+            return Created(Url.RouteUrl("GetProduct", new { id = result.Id }), result);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] Product product)
+        {
+            try
+            {
+                ProductService.UpdateProduct(id, product);
+                return Ok();
+
+            }
+            catch (UnkownProductException)
+            {
+                return NotFound();
+            }        
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            ProductService.DeleteProduct(id);
+            return Ok();
+        }
+    }
+}
---
+++ MiageCorp.AwesomeShop.ProductApi/MiageCorp.AwesomeShop.ProductApi/Data/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using MiageCorp.AwesomeShop.ProductApi.Models;
+
+namespace MiageCorp.AwesomeShop.ProductApi.Data
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+
+        Product? GetById(string id);
+        
+        void SaveProduct(Product product);
+
+        void DeleteProduct(string id);
+    }
+}
